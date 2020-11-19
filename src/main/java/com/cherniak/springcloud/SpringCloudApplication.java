8c6e6e31114a1a9f2f5d4880ff5365e120de60f9
Viktor Cherniak <barsetca@gmail.com>
2020-11-19 22:17:49 +0300

    Initial commit

---
+++ src/main/java/com/cherniak/springcloud/SpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.cherniak.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudApplication.class, args);
+  }
+
+}
